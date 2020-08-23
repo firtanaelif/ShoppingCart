ece711e2f2dd51f60ba942454c9366cd0001649e
Elif Fırtana <41020517+firtanaelif@users.noreply.github.com>
2020-08-23 20:04:33 +0300

    Update cartActions.js

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -27,7 +27,7 @@ const removeFromCart = (productId) => (dispatch, getState) => {
   dispatch({ type: CART_REMOVE_ITEM, payload: productId });
   const {
     cart: { cartItems },
-  } = getState;
+  } = getState();
   Cookie.set("cartItems", JSON.stringify(cartItems));
 };
 export { addToCart, removeFromCart };
